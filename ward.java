7d3e5036b49cc619e85ae139493461b4f96d947d
Stanley <stanleyobitunwase@gmail.com>
2024-04-15 13:56:53 +0100

    first commit

---
+++ ward.java
@@ -0,0 +1,31 @@
+package repo9;
+
+public class ward {
+    private String bed;
+    private String type_size;
+    private int size;
+
+    public String getBed() {
+        return bed;
+    }
+
+    public void setBed(String bed) {
+        this.bed = bed;
+    }
+
+    public String getType_size() {
+        return type_size;
+    }
+
+    public void setType_size(String type_size) {
+        this.type_size = type_size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
