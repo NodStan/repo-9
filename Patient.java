b26d2815486aeed32e27f40ef6bf1ebda27d03e5
Stanley <stanleyobitunwase@gmail.com>
2024-04-15 13:56:11 +0100

    first commit

---
+++ Patient.java
@@ -0,0 +1,34 @@
+package repo9;
+
+public class Patient {
+    private String name;
+    private String injury;
+
+
+//
+//
+//
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//
+//    public void setInjury(String injury){
+//        this.injury = injury;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getInjury() {
+//        return injury;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInjury() {
+        return injury;
+    }
+}
